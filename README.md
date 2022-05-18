0400ca1ce862ff3b1de8f829aa8ec5a3a064c49c
Sterling Baldwin <sterlingbaldwin@users.noreply.github.com>
2022-05-18 15:59:19 -0700

    mostly working

---
+++ README.md
@@ -0,0 +1,10 @@
+## Envirnment setup
+
+The dependecies for this are very light, Im only using pandas, numpy and gdal, but for convenience if you're missing something use the following to install them.
+
+    pip install -r requirements.txt
+
+## Usage
+
+python nvdi.py -i <input_file> -o <output_file> -t <threashold>
+
---
+++ ndvi.py
@@ -0,0 +1,91 @@
+import argparse
+import sys
+from pathlib import Path
+
+from enum import Enum
+
+class image_band(Enum):
+    costal_blue = 1
+    blue = 2
+    green_1 = 3
+    green = 4
+    yellow = 5
+    red = 6
+    red_edge = 7
+    nir = 8
+
+# set the default threshold to a small non-zero value
+DEFAULT_THRESHOLD = 0.1
+DESCRIPTION = "find the ndvi values from a GEOTIFF image, optionally filtering for a given threshold value"
+
+
+def find_ndvi(input_path: str, output_path: str, threshold: float):
+    # I like to put heavy inputs after the argument parsing is done,
+    # that way if a user does "-h" they dont have to wait for 
+    # modules to load
+    import numpy as np
+    from osgeo import gdal
+    
+    # load the dataset, and pull out the two bands we care about
+    dataset = gdal.Open(input_path, gdal.GA_ReadOnly)
+    nir = dataset.GetRasterBand(image_band.nir.value).ReadAsArray()
+    r   = dataset.GetRasterBand(image_band.red.value).ReadAsArray()
+
+    # find the ndvi values for the whole dataset, replacing
+    # nans with 0s. This will result in some divide by 0s
+    # so lets turn off that warning since it doesnt give
+    # us any meaningful information
+    np.seterr(invalid='ignore')
+    ndvi = np.nan_to_num((nir - r)/(nir + r), nan=0)
+
+    # apply our threshold
+    if(threshold != DEFAULT_THRESHOLD):
+        ndvi[ndvi < threshold] = 0
+    ndvi[ndvi > threshold] = 255
+    
+    rows, cols = ndvi.shape
+    driver = gdal.GetDriverByName("GTiff")
+    outdata = driver.Create(output_path, cols, rows, 1, gdal.GDT_UInt16)
+    outdata.SetGeoTransform(dataset.GetGeoTransform())
+    outdata.SetProjection(dataset.GetProjection())
+    outdata.GetRasterBand(1).WriteArray(ndvi)
+    outdata.FlushCache()
+
+    # TODO: figure out how to warp the projection correctly
+
+
+def main():
+    parser = argparse.ArgumentParser(description=DESCRIPTION)
+    parser.add_argument(
+        '--input_path', '-i',
+        type=str,
+        required=True,
+        help="Path to input file")
+    parser.add_argument(
+        '--output_path', '-o',
+        type=str,
+        help='Path to where output images should be stored, default = ./ndvi.tif',
+        default='ndvi.tif')
+    parser.add_argument(
+        '--threshold', '-t',
+        type=float,
+        default=DEFAULT_THRESHOLD,
+        help="filter the output image for values greater then the threshold")
+
+    args = parser.parse_args()
+    input_path = Path(args.input_path)
+    if(not input_path.is_file()):
+        print(f"Input file {input_path} does not exist!")
+        return -1
+    output_path = Path(args.output_path)
+    if(output_path.is_file()):
+        print(f"Output file {output_path} already exists, overwrite? y/[n]")
+        if(input() not in ['y', 'Y']):
+            return -1
+        # remove the previous output
+        output_path.unlink()
+    find_ndvi(str(input_path), str(output_path), args.threshold)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
